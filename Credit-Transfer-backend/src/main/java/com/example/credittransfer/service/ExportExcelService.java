5947245d67232f17ca4e26b951543483a887b9db
Jonathan Chen <sabza178@gmail.com>
2024-08-01 17:54:44 +0700

    Create export excel.

--- Credit-Transfer-backend/src/main/java/com/example/credittransfer/service/ExportExcelService.java
+++ Credit-Transfer-backend/src/main/java/com/example/credittransfer/service/ExportExcelService.java
@@ -1,7 +1,70 @@
 package com.example.credittransfer.service;
 
+import com.example.credittransfer.dto.response.TransferCreditResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class ExportExcelService {
+
+    private XSSFWorkbook workbook;
+    private XSSFSheet sheet;
+
+    private void createCell(Row row, int columnCount, Object value, CellStyle style) {
+        Cell cell = row.createCell(columnCount);
+        if (value instanceof Integer){
+            cell.setCellValue((Integer) value);
+        }else if (value instanceof Double){
+            cell.setCellValue((Double) value);
+        }else if (value instanceof Boolean){
+            cell.setCellValue((Boolean) value);
+        }else if (value instanceof Long){
+            cell.setCellValue((Long) value);
+        }else {
+            cell.setCellValue((String) value);
+        }
+        cell.setCellStyle(style);
+    }
+
+    private void createHeaderRow(List<String> headerList) {
+        sheet = workbook.createSheet("sheet 1");
+        Row row = sheet.createRow(0);
+        CellStyle cellStyle = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setBold(true);
+        font.setFontHeight(20);
+        cellStyle.setFont(font);
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+
+        int count = 0;
+        for(String header : headerList) {
+            createCell(row, count++, header, cellStyle);
+        }
+    }
+
+    private void writeData(List<TransferCreditResponse> transferCreditResponseList) {
+        int rowCount = 1;
+        CellStyle style = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setFontHeight(14);
+    }
+
+    public void exportDataToExcel(HttpServletResponse response, List<String> headerList, List<TransferCreditResponse> transferCreditResponseList) throws IOException {
+//        createHeaderRow(headerList);
+//        writeData();
+//        ServletOutputStream outputStream = response.getOutputStream();
+//        workbook.write(outputStream);
+//        workbook.close();
+//        outputStream.close();
+    }
 }
