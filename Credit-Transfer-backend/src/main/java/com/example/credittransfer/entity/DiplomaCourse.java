210b0d3e775b34b9a445644b6203f1f833b387e6
Jonathan Chen <sabza178@gmail.com>
2024-08-08 14:56:16 +0700

    Added JsonIgnore to entity.

--- Credit-Transfer-backend/src/main/java/com/example/credittransfer/entity/DiplomaCourse.java
+++ Credit-Transfer-backend/src/main/java/com/example/credittransfer/entity/DiplomaCourse.java
@@ -22,6 +22,7 @@ public class DiplomaCourse {
 
     private int dipCredit;
 
+    @JsonIgnore
     private boolean isActive;
 
     @ManyToOne
--- Credit-Transfer-backend/src/main/java/com/example/credittransfer/entity/UniversityCourse.java
+++ Credit-Transfer-backend/src/main/java/com/example/credittransfer/entity/UniversityCourse.java
@@ -1,5 +1,6 @@
 package com.example.credittransfer.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,6 +22,7 @@ public class UniversityCourse {
 
     private int uniCredit;
 
+    @JsonIgnore
     private boolean isActive;
 
 }
