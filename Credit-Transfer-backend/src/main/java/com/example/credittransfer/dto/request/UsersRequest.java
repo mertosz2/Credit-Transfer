c8d18f26293344095de59ef069e6889e9bc6eced
Jonathan Chen <sabza178@gmail.com>
2024-09-30 14:11:24 +0700

    change to thai text.

--- Credit-Transfer-backend/src/main/java/com/example/credittransfer/dto/request/UsersRequest.java
+++ Credit-Transfer-backend/src/main/java/com/example/credittransfer/dto/request/UsersRequest.java
@@ -1,5 +1,6 @@
 package com.example.credittransfer.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,16 +10,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UsersRequest {
 
-
+    @NotNull(message = "กรุณาใส่ชื่อบัญชีผู้ใช้")
     private String username;
 
+    @NotNull(message = "กรุณาใส่รหัสผ่าน")
     private String password;
 
+    @NotNull(message = "กรุณาใส่ชื่อจริง")
     private String firstName;
 
+    @NotNull(message = "กรุณาใส่นามสกุล")
     private String lastName;
 
+    @NotNull(message = "กรุณาใส่เบอร์โทรศัพท์")
     private String phone;
 
+    @NotNull(message = "กรุณาเลือกตำแหน่ง")
     private Integer role;
 }
